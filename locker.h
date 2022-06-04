af286e39d6032d9ed887dc52c940e56362bb9e60
lihao <18851197305@163.com>
2022-06-04 20:27:05 +0800

    将互斥锁与信号量封装到类中，使用信号量进行线程的唤醒与休眠，互斥锁进行代码保护

---
+++ locker.h
@@ -0,0 +1,64 @@
+#ifndef LOCKER_H
+#define LOCKER_H
+
+#include <exception>
+#include <pthread.h>
+#include <semaphore.h>
+
+class sem
+{
+public:
+    sem()
+    {
+        if( sem_init( &m_sem, 0, 0 ) != 0 )
+        {
+            throw std::exception();
+        }
+    }
+    ~sem()
+    {
+        sem_destroy( &m_sem );
+    }
+    bool wait()
+    {
+        return sem_wait( &m_sem ) == 0;
+    }
+    bool post()
+    {
+        return sem_post( &m_sem ) == 0;
+    }
+
+private:
+    sem_t m_sem;
+};
+
+class locker
+{
+public:
+    locker()
+    {
+        if( pthread_mutex_init( &m_mutex, NULL ) != 0 )
+        {
+            throw std::exception();
+        }
+    }
+    ~locker()
+    {
+        pthread_mutex_destroy( &m_mutex );
+    }
+    bool lock()
+    {
+        return pthread_mutex_lock( &m_mutex ) == 0;
+    }
+    bool unlock()
+    {
+        return pthread_mutex_unlock( &m_mutex ) == 0;
+    }
+
+private:
+    pthread_mutex_t m_mutex;
+};
+
+
+
+#endif
